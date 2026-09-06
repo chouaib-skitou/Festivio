040701c817ae2c3130ec5f24e9b7ab5fd425cd5f
ⵛⵄⵉⴱ <61671332+chouaib-skitou@users.noreply.github.com>
2026-09-06 16:38:11 +0200

    feat(api): apply task route rate limits

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -8,25 +8,28 @@ const {
 } = require('../controllers/taskController');
 const authMiddleware = require('../middlewares/authMiddleware');
 const authorize = require('../middlewares/authorize');
+const { readLimiter, writeLimiter } = require('../middlewares/rateLimiters');
 const { ROLES } = require('../constants/roles');
 
 const router = express.Router();
 const taskManagers = authorize(ROLES.ADMIN, ROLES.ORGANIZER_ADMIN);
 
-router.post('/', authMiddleware, taskManagers, createTask);
-router.get('/', authMiddleware, getAllTasks);
+router.post('/', authMiddleware, taskManagers, writeLimiter, createTask);
+router.get('/', authMiddleware, readLimiter, getAllTasks);
 router.put(
   '/:id',
   authMiddleware,
   authorize(ROLES.ADMIN, ROLES.ORGANIZER_ADMIN, ROLES.ORGANIZER),
+  writeLimiter,
   updateTask
 );
 router.patch(
   '/:id',
   authMiddleware,
   authorize(ROLES.ADMIN, ROLES.ORGANIZER_ADMIN, ROLES.ORGANIZER),
+  writeLimiter,
   patchTask
 );
-router.delete('/:id', authMiddleware, taskManagers, deleteTask);
+router.delete('/:id', authMiddleware, taskManagers, writeLimiter, deleteTask);
 
 module.exports = router;
